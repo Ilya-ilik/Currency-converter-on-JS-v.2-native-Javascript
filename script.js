e21b8cbc80173aa3948fe2abf386b37d0503cf1a
Ilya-ilik <101934601+Ilya-ilik@users.noreply.github.com>
2022-05-01 23:43:02 +0300

    Currency-converter-on-JS-v.2

---
+++ script.js
@@ -0,0 +1,14 @@
+//скрип конертации доллара в рубли 
+function moneyConverter1(valNum) {
+    document.getElementById("outputrub1").innerHTML = valNum * 74
+}
+
+//скрип конертации евро в рубли
+function moneyConverter2(valNum) {
+    document.getElementById("outputrub2").innerHTML = valNum * 81
+}
+
+//скрип конертации юаней в рубли
+function moneyConverter3(valNum) {
+    document.getElementById("outputrub3").innerHTML = valNum * 11
+}
